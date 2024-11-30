d99f81dae3b6ddf32a92cd3ab758a6b241222051
papemamadou <papesy303001@gmail.com>
2024-11-30 10:05:28 -0800

    tp

---
+++ Desktop/EXO SUR JAVA/HelloWorldWiththMethode.java
@@ -0,0 +1,9 @@
+public class HelloWorldWiththMethode {
+    public void Hello (){
+        System.out.println("you 're welcome!");
+    }
+    public static void main(String[] args) {
+        HelloWorldWiththMethode s = new HelloWorldWiththMethode();
+        s.Hello();
+    }
+}
